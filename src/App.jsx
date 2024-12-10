2907ee9500ce3e253c9dac18eb82c0ef7cf45ad7
yashvardhan8787 <139737436+yashvardhan8787@users.noreply.github.com>
2024-12-10 13:25:15 +0530

    Update App.jsx
    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-
-import { BrowserRouter, Routes , Route, Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
 import SearchResults from "./components/SearchResults";
-import VideoDetails from "./components/VideoDetails"
-import { AppContext} from "./context/contextApi";
+import VideoDetails from "./components/VideoDetails";
+import { AppContext } from "./context/contextApi";
 
 function App() {
-  
-
-  return (  
-     <AppContext>
-      <BrowserRouter basename="/youtube-clone">
-      <div className="flex flex-col h-full">
-      <Header />
-      <Routes>
-        <Route path="/youtube-clone"  excat element={<Feed />} ></Route>
-        <Route path="/searchResult/:searchQuery" element={<SearchResults />} ></Route>
-        <Route path="/video/:id" element={<VideoDetails />} ></Route>
-      </Routes>
-      </div>
-      </BrowserRouter>
-     </AppContext>
-  )
+  return (
+    <AppContext>
+      <Router>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/searchResult/:searchQuery" element={<SearchResults />} />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </Router>
+    </AppContext>
+  );
 }
 
-export default App
+export default App;
